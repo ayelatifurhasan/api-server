4acdc7d1fcf380125e35e3b2f85b2fd6234435a2
ayelatifurhasan <ayelatifurhasan2@gmail.com>
2026-03-27 15:44:45 +0600

    Update jwt.strategy.ts

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,15 +1,20 @@
-import { Strategy, ExtractJwt } from 'passport-jwt';
+import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
 
+@Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
-      super({
-            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-                  secretOrKey: process.env.JWT_SECRET,
-                      });
-                        }
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET || 'secret', // fallback
+    });
+  }
 
-                          validate(payload: any) {
-                              return payload;
-                                }
-                                }
+  async validate(payload: any) {
+    return {
+      userId: payload.id,
+      email: payload.email,
+    };
+  }
+}
