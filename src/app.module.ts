da9aa4637b329b26ee821183a398c07111acee25
ayelatifurhasan <ayelatifurhasan2@gmail.com>
2026-03-27 15:27:00 +0600

    Reorganize imports and add UsersModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,29 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-      TypeOrmModule.forRoot({
-            type: 'mysql',
-                  host: process.env.MYSQLHOST,
-                        port: parseInt(process.env.MYSQLPORT || '3306'),
-                              username: process.env.MYSQLUSER,
-                                    password: process.env.MYSQLPASSWORD,
-                                          database: process.env.MYSQLDATABASE,
-                                                autoLoadEntities: true,
-                                                      synchronize: true,
-                                                          }),
-                                                              AuthModule,
-                                                                ],
-                                                                })
-                                                                export class AppModule {}
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQLHOST,
+      port: parseInt(process.env.MYSQLPORT || '3306'),
+      username: process.env.MYSQLUSER,
+      password: process.env.MYSQLPASSWORD,
+      database: process.env.MYSQLDATABASE,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+
+    UsersModule,   // 🔥 MUST
+    AuthModule,    // 🔥 MUST
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
